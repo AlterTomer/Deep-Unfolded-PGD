0a9dcfa38f716e814a9637dc7db64328cb3dfcae
AlterTomer <134167637+AlterTomer@users.noreply.github.com>
2024-06-05 17:46:45 +0300

    Update PGDNet_AUX.py

--- PGDNet/PGDNet_AUX.py
+++ PGDNet/PGDNet_AUX.py
@@ -4,9 +4,6 @@ import matplotlib.pyplot as plt
 from time import time
 from PGD_AUXFunctions import find_capacity
 from random import randint, seed
-from itertools import chain
-
-# import scipy as sp
 
 
 # ------------------------------------- functions ----------------------------------------------------
@@ -52,13 +49,10 @@ def min_rate_vs_capacity(ch_n_valid, min_rate_v_arr, h_valid, k, sigma, name):
     plt.axhline(y=c_arr[2], linestyle='--', label=f'channel 3 grid capacity', color='blue')
     plt.plot(t_K, srv4, linestyle='dotted', color='black')  # label=f'Valid sum-rate channel: {n_arr[3]}'
     plt.axhline(y=c_arr[3], linestyle='--', label='channel 4 grid capacity', color='black')  # label=f'channel {
-    # n_arr[3]} capacity'
     plt.xlabel('Number of Iterations')
     plt.ylabel('Min Rate')
     plt.legend(loc='best')
-    # plt.title('Comparison of Different Valid Channels')
     plt.grid()
-    # plt.show()
     plt.savefig(f'capacity_{name}.jpg')
     plt.close()
 
@@ -78,82 +72,9 @@ def train_valid_loss(epochs, train_loss, valid_loss, name):
     plt.xlabel('Epochs')
     plt.ylabel('Loss (Negative Rate)')
     plt.legend(loc='best')
-    # plt.title('Train Loss vs Validation Loss')
     plt.grid()
-    # plt.show()
     plt.savefig(f'Loss_{name}.jpg')
     plt.close()
-
-
-def train_nn(epochs, H_train, H_valid, batch_size, model, K, optimizer, scheduler):
-    """
-    :param epochs: Number of training epochs
-    :param H_train: Training channels
-    :param H_valid: Validation channels
-    :param batch_size: Number of training channels in each batch
-    :param model: ANN
-    :param K: Number of PGD iterations for forward method
-    :param optimizer: Optimizer for learnable parameters
-    :param scheduler: Optimizer's learning rate scheduler
-    :return: Train loss, Validation loss, State of the best model
-    """
-    best_min_rate = 0
-    state = {}
-    train_loss = []
-    valid_loss = []
-    t0 = time()
-    for i in range(epochs):
-        t2 = time()
-        H_b = H_train[torch.randperm(H_train.size()[0])]
-        for b in range(0, len(H_b), batch_size):
-            data = H_b[b:b + batch_size]
-            x_n_arr, p_k, min_rate_arr = model.forward(K, data)
-            a_train = [model.min_rate(x_n, data) for x_n in x_n_arr]
-            b_train = [sum(r) for r in a_train]
-            c_train = [np.log2(2 + k) * b_train[k] for k in
-                       range(len(b_train))]  # log(2+k) because we count from 0, not 1
-            loss = -torch.sum(torch.stack(c_train)) / batch_size  # weighted loss to enhance the last PGD iterations
-
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-
-            # batch_min_rate = model.min_rate(p_k, data)
-            # loss = -batch_min_rate.mean()
-            # print(f"Epoch {i + 1}, Batch {int(b / batch_size) + 1}: Loss: {float(loss)}")  # (b / batch_size)
-
-        # average training loss after every epoch
-        _, p_k_t, min_rate_t_arr = model.forward(K, H_b)
-        min_rate_t = model.min_rate(p_k_t, H_b)
-        loss_t = -min_rate_t.mean().detach().numpy()
-        train_loss.append(loss_t)
-        # print(f'Epoch {i + 1} train loss = {loss_t}')
-
-        # average validation loss after every epoch
-        _, p_k_v, min_rate_v_arr = model.forward(K, H_valid)  # learned P_K and sum rate along PGD iterations
-        min_rate_v = model.min_rate(p_k_v, H_valid)
-        loss_v = -min_rate_v.mean().detach().numpy()
-
-        print(f'Epoch {i + 1} validation loss = {loss_v}')
-        if scheduler:
-            scheduler.step()  # loss_v
-
-        if -loss_v >= best_min_rate:
-            print('Improvement')
-            best_min_rate = -loss_v
-            state['epoch'] = i + 1
-            state['best_valid_rate'] = -loss_v
-            state['parameters'] = model.steps
-
-        valid_loss.append(loss_v)
-
-        t3 = time()
-        print(f'Epoch {i + 1} time is {(t3 - t2) / 60} minutes')
-
-    t1 = time()
-    print(f'Training and estimation time = {(t1 - t0) / 60} minutes')
-    return train_loss, valid_loss, state
-
 
 def rayleighFading(ch, N, channel_std):
     """
@@ -173,7 +94,8 @@ def rayleighFading(ch, N, channel_std):
     return h
 
 
-def AWGN(ch, N, noise_std):
+def AWGN(ch, N, noise_std, seed=0):
+    torch.manual_seed(seed)
     std_dev = noise_std / (2 ** 0.5)
     w_real = std_dev * torch.randn(ch, N)
     w_imag = std_dev * torch.randn(ch, N)
@@ -226,7 +148,8 @@ def train_general_net(epochs, A_train, A_valid, batch_size, model, K, optimizer,
                 H_est = torch.zeros_like(H_data)
                 if T == 1:
                     G_est = torch.reshape(BC_estimation(pilot_mat=pilot_mat, h=G_data.reshape((-1, 1)), h_std=h_std,
-                                                        w=AWGN(batch_size, M, w_std ** 2).T, w_std=w_std), G_data.size())
+                                                        w=AWGN(batch_size, M, w_std ** 2).T, w_std=w_std),
+                                          G_data.size())
                 else:
                     G_est = torch.zeros_like(G_data)
                     for g in range(len(G_data)):
@@ -241,7 +164,6 @@ def train_general_net(epochs, A_train, A_valid, batch_size, model, K, optimizer,
 
                 x_n_arr, p_k, min_rate_arr = model.forward(K, G_est, H_est)
 
-
             a_train = [model.min_rate(G_data, H_data, x_n[-T:] if T > 1 else x_n[-1:], x_n[:-T]) for x_n in x_n_arr]
             b_train = [torch.sum(r[1]) for r in a_train]
             c_train = [np.log2(2 + k) * b_train[k] for k in
@@ -249,7 +171,6 @@ def train_general_net(epochs, A_train, A_valid, batch_size, model, K, optimizer,
 
             loss_value = -torch.sum(torch.stack(c_train)) / batch_size
             train_loss.append(loss_value)  # weighted loss to enhance the last PGD iterations
-
 
             epoch_train_loss += model.min_rate(G_data, H_data, p_k[-T:] if T > 1 else p_k[-1:], p_k[:-T])[1].item()
 
@@ -260,7 +181,6 @@ def train_general_net(epochs, A_train, A_valid, batch_size, model, K, optimizer,
                 optimizer.step()
                 count = 0
                 train_loss = []
-
 
         # average training loss after every epoch
         epoch_train_loss /= -len(H_b)
@@ -334,6 +254,4 @@ def MAC_estimation(pilot_mat, h, h_std, w, w_std):
         cov_yy = var_h * (torch.matmul(pilot_mat, pilot_mat.T)) + (var_w + eps) * torch.eye(M)
     cov_mul = torch.matmul(cov_hy, torch.inverse(cov_yy))
     h_hat = torch.matmul(cov_mul, y)
-
     return h_hat
-
