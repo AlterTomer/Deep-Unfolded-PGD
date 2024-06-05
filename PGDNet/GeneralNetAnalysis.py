493d20a63b6203eb3b1cc9f2c7c4dbc72450889e
AlterTomer <134167637+AlterTomer@users.noreply.github.com>
2024-06-05 17:44:18 +0300

    Update GeneralNetAnalysis.py

--- PGDNet/GeneralNetAnalysis.py
+++ PGDNet/GeneralNetAnalysis.py
@@ -1,4 +1,6 @@
 from math import sqrt
+import numpy as np
+import torch
 from PGDNet_AUX import *
 from GeneralNet import PGDNet
 import torch.optim as optim
@@ -7,19 +9,19 @@ import os
 
 # MANET topology and channels
 T = 1  # transmitters
-M = 4  # relays
-N = 4  # end-users
+M = 3  # relays
+N = 3  # end-users
 sigma = 1  # noise variance
 w_std = sqrt(sigma)
 h_std = 1  # channel std
 ch_train = 1000
 ch_valid = 200
-#
-# # Initial power allocation
+
+# Initial power allocation
 phi = create_normalized_tensor(T, N)
 p = create_normalized_tensor(M, N)
-#
-# # Initial step sizes
+
+# Initial step sizes
 K = 40
 steps = torch.tensor([0.001] * K, requires_grad=True)  # 0.01
 
@@ -54,8 +56,7 @@ state['N'] = N
 state['K'] = K
 state['sigma'] = sigma
 state['h_std'] = h_std
-# -------------------------------------- Loss Graph and Data Saving
-# ----------------------------------------------------
+# -------------------------------------- Loss Graph and Data Saving ----------------------------------------------------
 if csi:
     dir_path = f"C:\\Users\\User\\Desktop\\MS.c\\MS.c\\research\\results\\General Net\\(T,M,N)={(T, M, N)} " \
                f"noise = {10 * np.log10(sigma)} dB, h_var={h_std ** 2}"
@@ -73,13 +74,19 @@ state_path = 'state.pickle'
 with open(state_path, 'wb') as file:
     pickle.dump(state, file)
 
-# -------------------------------------- Valid Min Rate Using Best Model
-# -----------------------------------------------
+# -------------------------------------- Valid Min Rate Using Best Model -----------------------------------------------
+state_path = r"C:\Users\User\Desktop\MS.c\MS.c\research\results\General Net\M = 3, N = 3, T = 1, noise = 0.0 " \
+             r"dB\state.pickle "
+file = open(state_path, 'rb')
+state = pickle.load(file)
 best_model = PGDNet(T, M, N, phi, p, state['parameters'], sigma)
 iters = 2000
 step_sizes = torch.tensor([0.001] * iters, requires_grad=False)
+phi = state['phi0']
+p = state['p0']
 classical_model = PGDNet(T, M, N, phi, p, step_sizes, sigma)
 
+A_valid = state['A_valid']
 G_valid = A_valid[:, :T * M]
 H_valid = A_valid[:, T * M:]
 
@@ -95,7 +102,8 @@ for i in range(len(A_valid)):
                                                                                                   i:i + batch_size].reshape(
             T, M),
                                                                                            H_valid[
-                                                                                           i:i + batch_size].reshape(M, N))
+                                                                                           i:i + batch_size].reshape(M,
+                                                                                                                     N))
     else:
         G_data = G_valid[i:i + batch_size].reshape(T, M)
         H_data = H_valid[i:i + batch_size].reshape(M, N)
@@ -118,9 +126,7 @@ for i in range(len(A_valid)):
 
         x_n_arr, p_k, min_rate_arr = best_model.forward(K, G_est, H_est)
 
-
         x_n_arr_classical, p_k_classical, min_rate_arr_classical = classical_model.forward(iters, G_est, H_est)
-
 
     mean_valid_learn[i] = np.reshape(min_rate_arr.detach().numpy(), -1)
     mean_valid_classical[i] = np.reshape(min_rate_arr_classical.detach().numpy(), -1)
@@ -128,21 +134,19 @@ for i in range(len(A_valid)):
 mean_valid_learn = np.mean(mean_valid_learn, axis=0)
 mean_valid_classical = np.mean(mean_valid_classical, axis=0)
 
-# ---------------------------- Classical vs. PGDNet
-# --------------------------------------------------------------------
+# ---------------------------- Classical vs. PGDNet --------------------------------------------------------------------
 
 tc = np.linspace(start=0, stop=iters, num=iters)
 tl = np.linspace(start=0, stop=K, num=K)
-
 plt.figure(figsize=(8, 6), dpi=80)
 plt.plot(tc, monotone_arr(mean_valid_classical), label='Classic PGD iterations', linestyle='dotted')
 plt.plot(tl, monotone_arr(mean_valid_learn), label='PGDNet iterations', linestyle='solid')
 
-plt.xlabel("Iterations")
-plt.ylabel("Min Rate")
+plt.xlabel("Iterations", fontsize=14)
+plt.ylabel("Min Rate", fontsize=14)
 plt.grid()
 plt.legend(loc='best')
-plt.savefig(f'mean min rate pgd {"CSI" if csi else "noisy"}.jpeg')
+plt.savefig(f'mean min rate pgd {"CSI" if csi else "noisy"} 133.jpeg')
 plt.close()
 print('done')
 print('****************************************************************************')
