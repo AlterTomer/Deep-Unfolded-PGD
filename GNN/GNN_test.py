a0b30fc8498a650915b6fff9c075f22d165dbeac
AlterTomer <134167637+AlterTomer@users.noreply.github.com>
2024-06-05 17:49:14 +0300

    Update GNN_test.py

--- GNN/GNN_test.py
+++ GNN/GNN_test.py
@@ -1,35 +1,58 @@
+import torch
 from torch.utils.data import DataLoader
-from PGDNet_AUX import rayleighFading, create_normalized_tensor
+from PGDNet_AUX import rayleighFading, create_normalized_tensor, BC_estimation, MAC_estimation, AWGN
 from GNN_AUX import *
 from GNN_Module import *
 import pickle
 import os
 
 
-#
+
 T = 1  # transmitters
-M = 2  # relays
-N = 2  # end-users
+M = 3  # relays
+N = 3  # end-users
 sigma = 1
-ch_train = 1000  # 1000
+w_std = sigma ** 0.5
+h_std = 1
+ch_train = 1000
 ch_valid = 200
 batch_size = 1
+csi = False
 
-# phi = create_normalized_tensor(T, N)
-# p = create_normalized_tensor(M, N)
 power_allocations = create_normalized_tensor(T + M + N, N)
-# power_allocations = torch.ones(T+M+N,N) * torch.tensor([0.5, np.sqrt(1 - (0.5 ** 2))])
-# power_allocations = power_allocations.float()
+file = open(r"C:\Users\User\Desktop\MS.c\MS.c\research\results\General Net\M = 3, N = 3, T = 1, noise = 0.0 "
+            r"dB\state.pickle", 'rb')
+PGD_state = pickle.load(file)
 
-# power_allocations = torch.cat([phi, p])
+A_train = PGD_state['A_train']
+A_valid = PGD_state['A_valid']
+if not csi:
+    G_train = A_train[:, :T * M]
+    H_train = A_train[:, T * M:]
+    A_train_est = torch.zeros_like(A_train)
+    for i in range(len(A_train)):
+        G_data = G_train[i:i + batch_size].reshape(T, M)
+        H_data = H_train[i:i + batch_size].reshape(M, N)
+        pilot_mat = torch.fft.fft(torch.eye(M)) / np.sqrt(M)
+        H_est = torch.zeros_like(H_data)
+        if T == 1:
+            G_est = torch.reshape(BC_estimation(pilot_mat=pilot_mat, h=G_data.reshape((-1, 1)), h_std=h_std,
+                                                w=AWGN(batch_size, M, w_std ** 2).T, w_std=w_std), G_data.size())
+        else:
+            G_est = torch.zeros_like(G_data)
+            for g in range(len(G_data)):
+                G_est[g] = torch.reshape(
+                    MAC_estimation(pilot_mat=pilot_mat, h=G_data[g].reshape((-1, 1)), h_std=h_std,
+                                   w=AWGN(batch_size, M, w_std ** 2).T, w_std=w_std), G_data[g].size())
 
-# A_train = torch.load('H_train53.pt')
-# A_valid = torch.load('H_valid53.pt')
-A_train = rayleighFading(ch_train, M * (T + N))
-A_valid = rayleighFading(ch_valid, M * (T + N))
+        for h in range(H_data.size()[1]):
+            H_est[:, h] = torch.reshape(
+                MAC_estimation(pilot_mat=pilot_mat, h=H_data[:, h].reshape((-1, 1)), h_std=h_std,
+                               w=AWGN(batch_size, M, w_std ** 2).T, w_std=w_std), H_data[:, h].size())
+        A_train_est[i] = torch.cat((G_est, H_est.resize(1, M * N)), dim=1)
 
 # Create a list of data samples
-train_data = [(create_manet_graph(A_train[i], power_allocations, T, M, N), power_allocations, A_train[i]) for i in range(ch_train)]
+train_data = [(create_manet_graph(A_train[i], power_allocations, T, M, N), power_allocations, A_train[i] if csi else A_train_est[i]) for i in range(ch_train)]
 valid_data = [(create_manet_graph(A_valid[i], power_allocations, T, M, N), power_allocations, A_valid[i]) for i in range(ch_valid)]
 
 # Create the dataset instance
@@ -45,20 +68,21 @@ out_feats = N  # Define the output feature size (power allocations)
 model = GNNModel(in_feats, hidden_feats, out_feats)
 
 # Perform the forward pass
-# new_power_allocations = model(g)  # , torch.cat([p, phi])
+
 optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
-epochs = 100
+epochs = 50
 train_loss_arr, valid_loss_arr, state = train_gnn(model, optimizer, epochs, batch_size, train_loader, valid_loader, T, M, N, sigma=sigma)
 state['in feats'] = in_feats
 state['hidden feats'] = hidden_feats
-model['out feats'] = out_feats
+state['out feats'] = out_feats
 state['p0'] = power_allocations
 
-dir_path = f"C:\\Users\\User\\Desktop\\MS.c\\MS.c\\research\\results\\GNN\\(T,M,N) = {(T, M, N)}, noise = {10 * np.log10(sigma)} dB test"
+dir_path = f"C:\\Users\\User\\Desktop\\MS.c\\MS.c\\research\\results\\GNN\\Noisy CSI\\(T,M,N) = {(T, M, N)}, noise = {10 * np.log10(sigma)} dB, h_var={h_std ** 2} "
 if not os.path.exists(dir_path):
     os.mkdir(dir_path)
 os.chdir(dir_path)
 state_path = 'state.pickle'
 with open(state_path, 'wb') as file:
     pickle.dump(state, file)
-train_valid_loss(epochs, train_loss_arr, valid_loss_arr, f'(T,M,N) = {(T, M, N)}, noise = {10 * np.log10(sigma)} dB')
+train_valid_loss(epochs, train_loss_arr, valid_loss_arr, f'(T,M,N) = {(T, M, N)}, noise = {10 * np.log10(sigma)} dB, '
+                                                         f'h_var={h_std ** 2}')
